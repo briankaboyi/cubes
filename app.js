554b2848c85dfbc8f6fa839e283465ab90939759
briankaboyi <briankaboyi0@gmail.com>
2023-11-03 19:03:51 +0300

    another commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cubeContainer = document.querySelector('.cube-container');
-// cube = document.querySelectorAll('.cube');
 console.log(cubeContainer);
+
+// mouse over/mouse out
 cubeContainer.addEventListener('mouseover', function (e) {
   const cube = e.target;
   cube.style.background = 'blue';
@@ -8,4 +9,16 @@ cubeContainer.addEventListener('mouseover', function (e) {
 cubeContainer.addEventListener('mouseout', function (e) {
   const cube = e.target;
   cube.style.background = 'red';
+});
+
+// click
+cubeContainer.addEventListener('click', function (e) {
+  const cube = e.target;
+  cube.classList.toggle('blue');
+  console.log('turned blue');
+  // if (cube.style.background === 'blue') {
+  //   cube.style.background = 'red';
+  // } else if (cube.style.background === 'red') {
+  //   cube.style.background = 'blue';
+  // }
 });
